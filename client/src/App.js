7885171bc3702227eadac299f6a9db7783f6b77d
Md. Abdur Rahim <rahim.cse.diu@gmail.com>
2026-05-12 18:19:56 +0600

    feat: Protect routes with AuthProvider and PrivateRoute
    Wrap App with AuthProvider, add /login route, and require login
    for HomePage and SingleProductPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,41 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage.js'; 
-import SingleProductPage from './pages/SingleProductPage';
-import './styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage.js";
+import SingleProductPage from "./pages/SingleProductPage";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
+import "./styles/App.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/:productId" element={<SingleProductPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/product/:productId"
+              element={
+                <PrivateRoute>
+                  <SingleProductPage />
+                </PrivateRoute>
+              }
+            />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
